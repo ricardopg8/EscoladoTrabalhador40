5fba70c018cc08b859028a02fe01e733edb1c657
ricardopg8 <50276326+ricardopg8@users.noreply.github.com>
2023-05-10 19:34:35 -0300

    Hello World

---
+++ Armazenar e Recuperar dados usando valores literais e variaveis em CSharp/TipodedadosInt.cs
@@ -0,0 +1,2 @@
+
+Console.WriteLine(123);
---
+++ HelloWorld/HelloWourld/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+
+Console.Write("This ");
+Console.Write("is");
+Console.Write(" the ");
+Console.Write("first ");
+Console.WriteLine("line.");
+Console.WriteLine("This is the second line.");
+
+Console.WriteLine(123);
+Console.WriteLine(12.3m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Beth";
+Console.WriteLine(firstName);
+firstName = "Conrad";
+Console.WriteLine(firstName);
+
+string value = "Um.Dois.Três.";
+Console.WriteLine(value);
+
+var message = "Hello world!";
+Console.WriteLine(message);
+
+
+
+// >>>>>>>>> ########## >>>>>>>>>> Exercício - Configurando e obtendo valores de variáveis <<<<<<<<<< ########## <<<<<<<<<<
+
+var texto = "Hello, ";
+Console.Write(texto);
+
+string nome = "Bob ";
+Console.Write(nome);
+
+int qtdInbox = 3;
+
+
+texto = "! ";
+texto = "You have ";
+Console.Write(texto);
+
+Console.Write(qtdInbox);
+
+texto = "in your inbox.   ";
+Console.Write(texto);
