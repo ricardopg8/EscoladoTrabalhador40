a0e113ff945e82462d0b3e3fe0ab408ccdc0e468
Ricardo Perez Gonçalves <ricardopg8@hotmail.com>
2023-05-19 19:29:36 -0300

    Recapitulação Concatenação

--- Executar_formatacao_Cadeia_Caracteres_Basica/Executar_formatacao_Cadeia_Caracteres_Basica/Program.cs
+++ Executar_formatacao_Cadeia_Caracteres_Basica/Executar_formatacao_Cadeia_Caracteres_Basica/Program.cs
@@ -68,3 +68,9 @@ Console.WriteLine($"view English output: \n\t\t{englishLocation}\n");
 string russianMessage = "\u041f\u043e\u0441\u043e\u0442\u0440\u0435\u0442\u044c \u0440\n";
 string russianLocation = $@"c:\Exercise\{projectName}\ru\RU\data.txt";
 Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+
+
+
+string primeiroNome = "Bob";
+string mensagem = "Olá " + primeiroNome;
+Console.WriteLine(mensagem);
